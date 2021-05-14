0858b9a991bff0112689e019974300b6e5b4a91b
Ankit Yadav <4nkitd@gmail.com>
2021-05-15 02:22:50 +0530

    link # removed

--- src/components/header.js
+++ src/components/header.js
@@ -2,17 +2,17 @@
 function Header() {
     return (
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="javascript:void(0);">TreeHouse</a>
+            <a class="navbar-brand" >TreeHouse</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarText">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="javascript:void(0);">Home</a>
+                        <a class="nav-link" >Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="javascript:void(0);">Github</a>
+                        <a class="nav-link" >Github</a>
                     </li>
                 </ul>
                 <span class="navbar-text">
