982c4e149b1a33694020b6dd1a2270d8bf662407
Ankit Yadav <4nkitd@gmail.com>
2021-05-15 02:27:41 +0530

    added links

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@
 function Header() {
     return (
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="">TreeHouse</a>
+            <a class="navbar-brand" href="/">TreeHouse</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
