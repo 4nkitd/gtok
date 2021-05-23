df797a7fea1da768a726461f159bbda974685e5c
Ankit Yadav <4nkitd@gmail.com>
2021-05-23 15:09:33 +0530

    removed react inder cards

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,9 @@
 import Header from './header';
 import Footer from './footer';
-import TinderCard from 'react-tinder-card'
 import db from './data'
+
+var repo_state = 0
+var repo = db[repo_state]
 
 const onSwipe = (direction) => {
     console.log('You swiped: ' + direction)
@@ -12,57 +14,59 @@ const onCardLeftScreen = (myIdentifier) => {
 }
 
 
-function Main() {
+function update_repo(repo, repo_state){
 
-    const git_repo = db
+    return repo[++repo_state]
+
+}
+
+function Main() {
 
     return (
         <main>
 
             <Header />
 
-            {
-                git_repo.map((repo) =>
-                <TinderCard
-                    onSwipe={onSwipe}
-                    onCardLeftScreen={() => onCardLeftScreen('fooBar')}
-                    preventSwipe={['right', 'left', 'bottom']}
-                >
+            <div>
 
-                    <div className="card-gtok">
-                        <div id="login-container">
+                <div className="card-gtok">
+                    <div id="login-container">
 
-                            <a target="_blank" href="{repo.url}" >
-                                <div className="profile-img"></div>
-                            </a>
+                        <a target="_blank" href="{g_info('url')}" >
+                            <div className="profile-img"></div>
+                        </a>
 
-                            <div className="col-12">
-                                    <h4 className="col-10 float-right text-right border-bottom" >
-                                        <a target="_blank" href="{repo.url}" > {repo.username} </a>
-                                    </h4>
-                                    <h3 className="col-12 text-right">{repo.name}</h3>
-                            </div>
-
-                            <div className="description col-12 card">
-                                {repo.description}
-                            </div>
-
-                            <footer className="p-2 pb-0">
-                                <div className="likes">
-                                    <p className="mb-0" >Stars</p>
-                                    <p className="mb-0" >{repo.stars}</p>
-                                </div>
-                                <div className="projects">
-                                    <p className="mb-0">Forks</p>
-                                    <p className="mb-0">{repo.forks}</p>
-                                </div>
-                            </footer>
+                        <div className="col-12">
+                            <h4 className="col-10 float-right text-right border-bottom" >
+                                <a target="_blank" href="{repo.url}" > {repo.username} </a>
+                            </h4>
+                            <h3 className="col-12 text-right">{repo.name}</h3>
                         </div>
+
+                        <div className="description col-12 card">
+                            {repo.description}
+                        </div>
+
+                        <footer className="p-2 pb-0">
+                            <div className="likes">
+                                <p className="mb-0" >Stars</p>
+                                <p className="mb-0" >{repo.stars}</p>
+                            </div>
+                            <div className="projects">
+                                <p className="mb-0">Forks</p>
+                                <p className="mb-0">{repo.forks}</p>
+                            </div>
+                        </footer>
+
+                        <button onClick={ () => { repo = repo[++repo_state] } } className="btn btn-sm btn-block" > NEXT </button>
+
                     </div>
 
-                </TinderCard>
+                </div>
 
-                )}
+
+            </div>
+
 
 
             <Footer />
